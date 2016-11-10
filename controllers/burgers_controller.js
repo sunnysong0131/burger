f880d69ba88fadd8a0a0ab1fd516a67cad677544
Yuhan Song <yuhan.song@rutgers.edu>
2016-11-10 17:53:26 -0500

    sfbasfdsa

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,36 @@
+/*
+Here is where you create all the functions that will do the routing for your app, and the logic of each route.
+*/
+var express = require('express');
+var router = express.Router();
+var cat = require('../models/burger.js');
+
+router.get('/', function (req, res) {
+	res.redirect('/burgers');
+});
+
+router.get('/burgers', function (req, res) {
+	cat.all(function (data) {
+		var hbsObject = { burgers: data };
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/burgers/create', function (req, res) {
+	cat.create(['burger_name'], [req.body.burger_name], function () {
+		res.redirect('/burgers');
+	});
+});
+
+router.put('/burgers/update/:id', function (req, res) {
+	var condition = 'id = ' + req.params.id;
+
+	console.log('condition', condition);
+
+	cat.update({ devoured: req.body.sleepy }, condition, function () {
+		res.redirect('/burgers');
+	});
+});
+
+module.exports = router;
