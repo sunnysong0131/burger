ed5b2efcdb4d0ad5d35680560f2aab5fc93b8a4f
Yuhan Song <yuhan.song@rutgers.edu>
2016-11-10 17:36:04 -0500

    whatever

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Beef');
+INSERT INTO burgers (burger_name) VALUES ('Chicken');
+INSERT INTO burgers (burger_name) VALUES ('Pork');
+INSERT INTO burgers (burger_name) VALUES ('Vegen');
